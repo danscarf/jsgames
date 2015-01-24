01067a66af696c4d2a7026e9c1c2ece3af4b65c6
roxydoggy <roxydoggy@users.noreply.github.com>
2015-01-23 23:58:06 -0800

    more changes

---
+++ pewpew/website/Script/global.js
@@ -0,0 +1,13 @@
+﻿var context;
+var canvas;
+var FPS = 30;
+var setIntervalId;
+var CANVAS_WIDTH = 600;
+var CANVAS_HEIGHT = 400;
+
+var PLAYER_MOVE_DISTANCE = 3;
+var PLAYER_START_POSITION = 100;
+
+
+var textX = 50;
+var textY = 50;
