69bff498e7184a38d494f0559d50e7aba121f573
Dan Scarf <dscarf@gmail.com>
2015-09-03 15:59:47 -0700

    Adding more stuff.

--- artillery/www/js/states/boot.js
+++ artillery/www/js/states/boot.js
@@ -7,6 +7,9 @@ function Boot() {
 Boot.prototype = {
   preload: function() {
     this.load.image('preloader', 'assets/preloader.gif');
+    this.load.image('bullet', 'assets/games/asteroids/bullets.png');
+    this.load.image('ship', 'assets/games/asteroids/ship.png');
+
   },
   create: function() {
     this.game.input.maxPointers = 1;
