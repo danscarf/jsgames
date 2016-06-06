259be8070575d429989ec8130051c36f80e61f6b
roxydoggy <roxydoggy@users.noreply.github.com>
2016-06-05 18:01:59 -0700

    Initial commit for change from 'High Scores' to Leaderboard.

---
+++ artillery/www/js/states/leaderboard.js
@@ -0,0 +1,23 @@
+﻿'use strict';
+
+function Leaderboard() { }
+
+Leaderboard.prototype = {
+    create: function () {
+
+        var style = { font: '32px Arial', fill: '#ffffff', align: 'center' };
+        this.GameOverText = this.game.add.text(this.game.world.centerX, 100, 'View some high scores!', style);
+        this.GameOverText.anchor.setTo(0.5, 0.5);
+
+        this.mainMenuText = this.game.add.text(this.game.world.centerX, 200, 'Main Menu', { font: '32px Arial', fill: '#ffffff', align: 'center' });
+        this.mainMenuText.anchor.setTo(0.5, 0.5);
+        this.mainMenuText.inputEnabled = true;
+        this.mainMenuText.events.onInputDown.add(this.mainMenu, this);
+    },
+
+    update: function () {
+    },
+    mainMenu: function () {
+        this.game.state.start('menu');
+    }
+}
